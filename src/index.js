8a3c1b3bf7f3d03eb0ddebfaaa419d5d2a154035
shnhrrsn <shnhrrsn@gmail.com>
2018-10-23 21:41:50 -0400

    Added support for jsx files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
 const path = require('path')
+const processor = {
+
+	preprocess(text, filename) {
+		return [ text.replace(/(^\s*)import\s+'([^']+)'(\s*$)/gm, (original, leading, importPath, trailing) => {
+			if(importPath.indexOf('/') < 0) {
+				return original;
+			}
+
+			const name = path.basename(importPath)
+
+			if(name.indexOf('.') >= 0) {
+				return original
+			}
+
+			return `${leading}import { ${name} } from '${importPath}'${trailing}`
+		}) ]
+	},
+
+	postprocess(messages, filename) {
+		return messages.reduce((a, b) => a.concat(b))
+	}
+
+}
 
 module.exports = {
 
 	processors: {
-		'.js': {
-			preprocess(text, filename) {
-				return [ text.replace(/(^\s*)import\s+'([^']+)'(\s*$)/gm, (original, leading, importPath, trailing) => {
-					if(importPath.indexOf('/') < 0) {
-						return original;
-					}
-
-					const name = path.basename(importPath)
-
-					if(name.indexOf('.') >= 0) {
-						return original
-					}
-
-					return `${leading}import { ${name} } from '${importPath}'${trailing}`
-				}) ]
-			},
-
-			postprocess(messages, filename) {
-				return messages.reduce((a, b) => a.concat(b))
-			}
-		}
+		'.js': processor,
+		'.jsx': processor
 	}
 
 }
